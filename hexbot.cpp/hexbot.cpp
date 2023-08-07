83ce8a04adc8a361780ca95ea1fc7e8ca6f71c8f
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-08-07 15:14:49 +0530

    Create hexbot.cpp

---
+++ hexbot.cpp/hexbot.cpp
@@ -0,0 +1,13 @@
+/*
+| rumor — by rumor, Inc.
+| ~~~~~~~~~~~~~~~~~~~~~~
+| Principal Architect
+| Author
+| Prabhat Kumar.
+*/
+#include <iostream>
+int main() {
+    // Write C++ code here. 
+    std::cout << "Hello world, I am hexbot!";
+    return 0;
+}
