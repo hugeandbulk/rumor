6af0f258d7abdc9fe9a0bad7a43ac821e78cd059
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-08-07 21:51:59 +0530

    Update rumor.cpp

--- rumor.cpp
+++ rumor.cpp
@@ -1,9 +1,21 @@
 /*
-| rumor — by rumor, Inc.
-| ~~~~~~~~~~~~~~~~~~~~~~
-| Principal Architect
-| Author
-| Prabhat Kumar.
+|--------------------------------------------------------------------------
+| rumor — by rumor, Inc. )):
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create rumor application.
+| CEO //
+| Concept & Idea //
+| Principal Architect //
+| Creator   : ¦  Prabhat Kumar  ¦
+|--------------------------------
+| Copyright : ¦  Prabhat Kumar  ¦
+|--------------------------------
+| Company   : ¦  Infinity, Inc. ¦
+|--------------------------------
+| COO       : ¦  Zareen Khan    ¦
+|
+| Dated — 6nd August, 2023 
 */
 #include <iostream>
 int main() {
