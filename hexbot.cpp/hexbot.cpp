579cfdbbeca502c6e1b9abf9ef54e81292aebe14
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-08-08 15:45:58 +0530

    Update hexbot.cpp

--- hexbot.cpp/hexbot.cpp
+++ hexbot.cpp/hexbot.cpp
@@ -1,9 +1,23 @@
 /*
-| rumor — by rumor, Inc.
-| ~~~~~~~~~~~~~~~~~~~~~~
-| Principal Architect
-| Author
-| Prabhat Kumar.
+|--------------------------------------------------------------------------
+| rumor — by rumor, Inc. )):
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create rumor application.
+| CEO //
+| Concept & Idea //
+| Principal Architect //
+| Creator   : ¦  Prabhat Kumar  ¦
+|--------------------------------
+| Copyright : ¦  Prabhat Kumar  ¦
+|--------------------------------
+| Company   : ¦  Infinity, Inc. ¦
+|--------------------------------
+| COO       : ¦  Zareen Khan    ¦
+|--------------------------------
+| bot       : ¦  hexbot         ¦
+|
+| Dated — 6th August, 2023 
 */
 #include <iostream>
 int main() {
